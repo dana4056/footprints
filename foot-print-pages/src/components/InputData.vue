0fed8247b45557dd635227bda855e8e3d5ef6ac3
dana <dana4056@naver.com>
2022-04-18 16:41:20 +0900

    회원가입 폼 유효성 검사

--- foot-print-pages/src/components/InputData.vue
+++ foot-print-pages/src/components/InputData.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="inputData">
 
-    <form>
+    <form @submit.prevent="onSubmit">
     <div class="inputDiv">
-      <label for="userId">아이디</label>
+      <label>아이디</label>
       <div class="idGroup">
-        <span><input id='userId1' class="idItem" v-model="Id1" type="text" placeholder="아이디"></span>
+        <span><input id='userId1' class="idItem" v-model="Id1" type="text" placeholder="아이디" required></span>
         <span class="idItem">@</span>
         <span class="idItem">
           <select name="domain" id='userId2' v-model="Id2">
@@ -17,23 +17,34 @@
       </div>
     </div>
 
-    <div class="inputDiv">
-      <label for="password1">비밀번호</label>
-      <input id='password1' v-model="Pw1" type="password" placeholder="비밀번호 (영어, 숫자, 특수문자 포함 8~20자)">
+    <div class="inputDiv" v-bind:class="{errorType:isDirrentPw}">
+      <label>비밀번호</label>
+      <input id='password1' 
+      v-on:focusout="checkPw"
+      v-model="Pw1" type="password" 
+      placeholder="비밀번호 (영어, 숫자, 특수문자 포함 8~20자)" 
+      required>
+    </div>
+
+    <div class="inputDiv" v-bind:class="{errorType:isDirrentPw}">
+      <label>비밀번호 확인</label>
+      <input id='password2' 
+      v-on:focusout="checkPw"
+      v-model="Pw2" type="password" 
+      placeholder="비밀번호 재입력" required>
+      <span class="errortype" v-if="isDirrentPw">비밀번호가 일치하지 않습니다.</span>
+    </div>
+
+    <div class="inputDiv" v-bind:class="{errorType:isValidNick()}">
+      <label>닉네임</label>
+      <input id='nickname' 
+      v-model="Nick" type="text" 
+      placeholder="별명 (2~8자)" required>
+      <span class="errortype" v-if="isValidNick()">닉네임은 2~8글자이어야 합니다.</span>
     </div>
 
     <div class="inputDiv">
-      <label for="password2">비밀번호 확인</label>
-      <input id='password2' v-model="Pw2" type="password" placeholder="비밀번호 재입력">
-    </div>
-
-    <div class="inputDiv">
-      <label for="nickname">닉네임</label>
-      <input id='nickname' v-model="Nick" type="text" placeholder="별명 (2~8자)">
-    </div>
-
-    <div class="inputDiv">
-      <label for="userPhone">휴대폰 번호</label>
+      <label>휴대폰 번호</label>
       <div class="phoneGroup">
         <span class="phoneItem"><input id='userPhone' v-model="Phone" type="text" placeholder="전화번호"></span>
         <span class="phoneItem"><button class="btn">인증번호 받기</button></span>
@@ -45,18 +56,17 @@
     </div>
 
     <div class="inputDiv">
-      <label for="userPhone">지역설정</label>
+      <label>지역설정</label>
       <input id='userArea' v-model="Area" type="text" placeholder="지역명(ex. 성북구 정릉동)">
       <input id='RuserArea' type="text">
     </div>
 
-    <button v-on:click.prevent="submitData">회원가입하기</button>
+    <button type="submit" v-on:click.prevent="submitData">회원가입하기</button>
     </form>
   </div>
 </template>
 
 <script>
-
 export default {
   data(){
     return {
@@ -67,21 +77,57 @@ export default {
       Nick: "",
       Phone: "",
       Area: "",
+      isDirrentPw: false
     }
   },
   methods:{
     submitData(){
-      const Member = {
-        Id: this.Id1+"@"+this.Id2,
-        Pw: this.Pw1,
-        Nick: this.Nick,
-        Phone: this.Phone,
-        Area: this.Area
+      if(this.isValidAll()){
+        const Member = {
+          Id: this.Id1+"@"+this.Id2,
+          Pw: this.Pw1,
+          Nick: this.Nick,
+          Phone: this.Phone,
+          Area: this.Area
+        }
+        console.log(Member);
+        this.$store.commit('addOneMember', Member);
       }
-
-      console.log(Member);
-      this.$store.commit('addOneMember', Member);
-      
+      else{
+        alert("입력한 정보들을 확인해주세요");
+      }
+    },
+    isValidAll(){  // 최종 양식 확인
+      if(this.Id1 != "" && this.Id2 != "" && this.Pw1 != "" && this.Pw2 != "" &&
+      this.Nick != "" && this.Phone != "" && this.Area != ""){
+        if(!this.isValidNick() && !this.isDirrentPw){
+          return true;
+        }
+        return false;
+      }
+      else{
+        return false;
+      }
+    },
+    checkPw(){      // 비밀번호 & 비밀번호 확인란 일치 여부
+      if(this.Pw1 != "" && this.Pw2 != ""){
+        if(this.Pw1 != this.Pw2){
+          this.isDirrentPw = true;
+        }
+        else{
+          this.isDirrentPw = false;
+        }
+      }
+    },
+    isValidNick(){   // 닉네임 형식 판단
+      if(this.Nick != ""){
+        if(this.Nick.length >=2 && this.Nick.length <= 8){
+          return false;
+        }
+        else{
+          return true;
+        }
+      }
     }
   }
 }
@@ -121,6 +167,17 @@ input, select{
 input:focus{
   outline: none;
   border-color:#737beb;
+  background: #e8f0fe;
+}
+.errorType input{
+  background: #fff6f6;
+  outline: none;
+  border-color:#eb7373;
+}
+.errorType span{
+  text-align: left;
+  font-size: 12px;
+  color: #eb7373;
 }
 button{
     width: 100%;
