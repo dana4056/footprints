4f90da87ad3c4bd14e671613814f1dcdef70fd4f
sunjinb <tjswls8062@skuniv.ac.kr>
2022-07-23 17:09:21 +0900

    220723

--- FootPrints/src/main/java/footprints/footprints/controller/post/PostController.java
+++ FootPrints/src/main/java/footprints/footprints/controller/post/PostController.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
