e108ed9976663999bf10298ad62d38a2369b27d3
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-22 21:18:49 +0900

    지원 merge2

--- FootPrints/src/main/java/footprints/footprints/repository/roomInfo/RoomInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package footprints.footprints.repository.roomInfo;
-
-public interface RoomInfoRepository {
-    void save_d(String nick, Long post_id);
-    void delete_d(String nick, Long post_id);
-    void delete_all(Long post_id);
-}
