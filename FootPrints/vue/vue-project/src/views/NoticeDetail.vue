393b3cbaac1b5158a2ce087464422470494031c0
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-31 00:42:00 +0900

    220831 공지사항 UI 수정

--- FootPrints/vue/vue-project/src/views/NoticeDetail.vue
+++ FootPrints/vue/vue-project/src/views/NoticeDetail.vue
@@ -2,17 +2,16 @@
   <div>
     <tool-bar></tool-bar>
     <div id="wrap">
-      <div id="header">
-        {{ this.$store.state.notice.title }}
-        <div id="date">
-          {{ this.$store.state.notice.post_time }}  
+      <div class="header">
+        <img src="../assets/notice.png">
+        <h2 id="title">{{ this.$store.state.notice.title }}</h2>
+        <div class="info">
+          <p id="view">{{ this.$store.state.notice.post_time }}</p>
+          <p id="date">조회수 {{ this.$store.state.notice.view_num }}</p>
         </div>
       </div>
-      <div id="content">
-        {{ this.$store.state.notice.content }}
-      </div>
-      <div id="view_count">
-        {{ this.$store.state.notice.view_num}}
+      <div class="content">
+        <p v-for="c in content" v-bind:key="c">{{c}}</p>
       </div>
     </div>
     <footer-area id="footer"></footer-area>
@@ -29,8 +28,16 @@ components:{
     ToolBar,
     FooterArea,
   },
+  data() {
+    return {
+      content: ""
+    }
+  },
 created(){
   const notice_id = this.$route.params.id;
+
+  this.content = this.$store.state.notice.content.split('\n');
+  
   this.$store.dispatch('FETCH_NOTICE_DETAIL', notice_id);
   setTimeout(() => { 
     return this.$store.state.notice;
@@ -40,6 +47,45 @@ created(){
 }
 </script>
 
-<style>
-
+<style scoped>
+#wrap {
+  margin: 50px auto;
+  width: 700px;
+  height: 600px;
+  border: 2px solid rgba(0, 0, 0, 0.558);
+  border-radius: 40px;
+}
+img {
+  margin: 32px 0 0 25px;
+  height: 40px;
+  width: 40px;
+}
+.header {
+  height: 100px;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.558);
+}
+.header > * {
+  float: left;
+}
+#title {
+  font-family: 'Noto Sans KR', sans-serif;
+  width: 450px;
+  margin: 33px 0 0 20px;
+  text-align: left;
+}
+.info {
+  width: 150px;
+}
+#view, #date {
+  width: 100%;
+  margin: 22px 0 0 0 ;
+  font-size: 14px;
+  color: #666;
+}
+.content {
+  padding: 5px 20px;
+  text-align: left;
+  height: 460px;
+  overflow: auto;
+}
 </style>
