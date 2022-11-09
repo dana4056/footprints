ffa5b2a9914ac29842b12f30c227f666467da410
sunjinb <tjswls8062@skuniv.ac.kr>
2022-11-09 20:08:48 +0900

    221109 before merge

--- FootPrints/vue/vue-project/src/views/SignUpView.vue
+++ FootPrints/vue/vue-project/src/views/SignUpView.vue
@@ -8,14 +8,6 @@
     </header>
     <div id="wrap">
       <h3>회원가입</h3>
-      <div class="SnsBox">
-        <div class="tinytext">SNS로 간편 회원가입</div>
-        <ul>
-          <li><img class="brandLogo" src="../assets/kakaoLogo.png"></li>
-          <li><img class="brandLogo" src="../assets/NaverLogo.png"></li>
-          <li><img class="brandLogo" src="../assets/facebookLogo.png"></li>
-        </ul>
-      </div>
       <!-- 일반 회원가입 form -->
       <div id="inputData">
         <form>
