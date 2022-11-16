0cf4ea0bc56ffb67e53f8affe4f721a4b757f397
sunjinb <tjswls8062@skuniv.ac.kr>
2022-11-16 14:30:49 +0900

    로컬에서 돌아갈 수 있는 환경 구성

--- FootPrints/vue/vue-project/src/api/index.js
+++ FootPrints/vue/vue-project/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. HTTP Request & Response와 관련된 기본 설정
 const config = {
-    baseUrl:"http://3.37.153.14:8080"
+    baseUrl:"http://localhost:8080"
 };
 
 ////////////////////////// TOKEN //////////////////////////
